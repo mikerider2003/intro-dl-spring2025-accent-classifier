22d6c0b8a61c2cd32a0920b49c19d834d579fb63
Michal Stetina <u890345@byzantium.uvt.nl>
2025-04-24 13:32:56 +0200

    First run on cluster

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -p GPU              # partition (queue)
+#SBATCH -N 1                # number of nodes
+#SBATCH -t 0-36:00          # time (D-HH:MM)
+#SBATCH -o slurm.%N.%j.out  # STDOUT
+#SBATCH -e slurm.%N.%j.err  # STDERR
+#SBATCH --gres=gpu:1        # request 1 GPU
+
+# Setup Conda environment
+if [ -f "/usr/local/anaconda3/etc/profile.d/conda.sh" ]; then
+    . "/usr/local/anaconda3/etc/profile.d/conda.sh"
+else
+    export PATH="/usr/local/anaconda3/bin:$PATH"
+fi
+
+# Activate your conda environment
+source activate env1
+
+# Navigate to your project directory
+cd ~/intro-dl-spring2025-accent-classifier
+
+# Run your training script
+python train_1d_raw.py
+
